9b5d67798b6ba85b28c9ad1e2ce822bacfb00636
dtharris95 <dtharris95@gmail.com>
2024-10-05 16:26:51 -0400

    Made changes in GitHub, cloned repo, deleted locally hosted one, reconfiguring git and github in this project.

---
+++ src/components/Cards/cards.js
@@ -0,0 +1,91 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import "./cards.scss";
+import comingSoon from "../../assets/images/comingSoon.png";
+import HKTIcon from "../../assets/images/HKTIcon.png";
+
+function BasicExample() {
+  return (
+    <>
+      <div className="flex-container">
+        <div className="card-container">
+          {/* Harris Kitchen Table */}
+          <a href="https://harriskitchentable.com">
+            <Card.Img variant="top" className="project-images" src={HKTIcon} />
+          </a>
+          <Card.Body>
+            <Card.Title className="card-title">Harris Kitchen Table</Card.Title>
+            <Card.Text className="card-text">
+              Template website serving as proof-of-concept for potential
+              freelance restaurant clients.
+            </Card.Text>
+            <Card.Text className="card-skills">
+              React | JavaScript | HTML | CSS | PocketBase
+            </Card.Text>
+            {/* <Button variant="primary">Visit Site</Button> */}
+          </Card.Body>
+
+          {/* Hinge To Harris - https://hingetoharris.com */}
+          <a href="">
+            <Card.Img
+              variant="top"
+              className="project-images"
+              src={comingSoon}
+            />
+          </a>
+          <Card.Body>
+            <Card.Title>Hinge To Harris</Card.Title>
+            <Card.Text className="card-text">
+              {" "}
+              A website where guests can find information, RSVP, and view the
+              registry for my wedding!
+            </Card.Text>
+            <Card.Text className="card-skills">WordPress</Card.Text>
+            {/* <Button variant="primary">Visit Site</Button> */}
+          </Card.Body>
+
+          {/* Budget Builder */}
+          <a href="">
+            <Card.Img
+              variant="top"
+              className="project-images"
+              src={comingSoon}
+            />
+          </a>
+          <Card.Body>
+            <Card.Title>Budget Builder</Card.Title>
+            <Card.Text className="card-text">
+              {" "}
+              A user-friendly web application where users can budget and track
+              monthly income, expenses, and disrectionary spending.
+            </Card.Text>
+            <Card.Text className="card-skills">C# | .NET</Card.Text>
+            {/* <Button variant="primary">Visit Site</Button> */}
+          </Card.Body>
+
+          {/* Veterinarian Site */}
+          <a href="">
+            <Card.Img
+              variant="top"
+              className="project-images"
+              src={comingSoon}
+            />
+          </a>
+          <Card.Body>
+            <Card.Title>Veterinarian Site</Card.Title>
+            <Card.Text className="card-text">
+              {" "}
+              Template website serving as proof-of-concept for potential
+              freelance veterinarian clients.
+            </Card.Text>
+            <Card.Text className="card-skills">TBD</Card.Text>
+            {/* <Button variant="primary">Visit Site</Button> */}
+          </Card.Body>
+          {/* </Card> */}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default BasicExample;
---
+++ src/components/Layout/index.js
@@ -0,0 +1,14 @@
+import Homepage from "../Homepage";
+import Navbar from "../Navbar";
+import "./layout.scss";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Homepage />
+    </>
+  );
+};
+
+export default Layout;
